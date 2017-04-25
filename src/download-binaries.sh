3ba1fc1a875d0c962d9c9742a738ee4c62ab85a7
Sylvain Marty <sylvain.marty@aol.fr>
2017-04-25 08:03:27 +0200

    deleteion bad files

--- src/download-binaries.sh
+++
@@ -1,8 +0,0 @@
-cd packaging/jekyll/
-curl -L -O --fail https://github.com/L-A/Traveling-Jekyll/releases/download/3.4.3/traveling-jekyll-3.4.3-linux-x86.tar.gz
-curl -L -O --fail https://github.com/L-A/Traveling-Jekyll/releases/download/3.4.3/traveling-jekyll-3.4.3-linux-x86_64.tar.gz
-curl -L -O --fail https://github.com/L-A/Traveling-Jekyll/releases/download/3.4.3/traveling-jekyll-3.4.3-osx.tar.gz
-
-mv traveling-jekyll-3.4.3-linux-x86.tar.gz traveling-jekyll-linux-x86.tar.gz
-mv traveling-jekyll-3.4.3-linux-x86_64.tar.gz traveling-jekyll-linux-x86_64.tar.gz
-mv traveling-jekyll-3.4.3-osx.tar.gz traveling-jekyll-osx.tar.gz
